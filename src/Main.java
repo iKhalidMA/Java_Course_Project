f501d5c6f38c55b96e2e450a6243d5fe6fc5b825
iKhalidMA <khaled.maher.abdelwahabu@gmail.com>
2023-02-17 00:14:20 +0200

    For Saving My java Course Program!

--- src/Main.java
+++ src/Main.java
@@ -10,26 +10,26 @@ public class Main {
             outputs > salary after taxes,  netSalary
         */
         Scanner salariesDetails = new Scanner(System.in);
-        //
+        ///
         float taxesPercentage = 0.3f;
         double baseSalary, incentives, taxes, workPerHour, BonusHour, netSalary = 0;
-        //
+        ///
         System.out.print("Please dear employee, Enter your base Salary for some operations: ");
         baseSalary = salariesDetails.nextDouble();
         System.out.print("Please dear employee, Enter your incentive per hour for some operations: ");
         workPerHour = salariesDetails.nextDouble();
         System.out.print("Please dear employee, Enter your additions hours for some operations: ");
         BonusHour = salariesDetails.nextDouble();
-        //
+        ///
         taxes = baseSalary * taxesPercentage;
         incentives = workPerHour * BonusHour;
         netSalary = (baseSalary + incentives) - taxes;
-        //
+        ///
         System.out.println("your base Salary is equal to: \t" + baseSalary);
         System.out.println("Taxes \t" + taxes);
         System.out.println("Salary after taxes: \t" + (baseSalary - taxes));
         System.out.println("Incentive \t" + incentives);
-        //
+        ///
         System.out.println("-----------------");
         System.out.println("Net Salary:\t " + netSalary + "EGP");
 
