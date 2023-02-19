608ec49bc8177a541387aeed479b32ef0ca90360
iKhalidMA <khaled.maher.abdelwahabu@gmail.com>
2023-02-19 12:01:48 +0200

    for Saving java course program , take a look for Explore my first step in prog.

--- src/Main.java
+++ src/Main.java
@@ -9,20 +9,33 @@ public class Main {
             processes > taxes = baseSalary * taxesPercentage && netSalary = (baseSalary + incentives )-taxes
             outputs > salary after taxes,  netSalary
         */
+        /* Additional Tasking:
+            1- let the taxPercent .5 -> when the base salary equal or more than 30K
+            2- if the employee worked more than 15 h -> his normal working incentives  + 500
+            3- Target Details for annual multiply by 0.05 for each month
+         */
         Scanner salariesDetails = new Scanner(System.in);
-        ///
-        float taxesPercentage = 0.3f;
-        double baseSalary, incentives, taxes, workPerHour, BonusHour, netSalary = 0;
+//        ///
+        double baseSalary, taxPercent = 0, incentives, taxes, workPerHour, BonusHour, netSalary = 0.0;
         ///
         System.out.print("Please dear employee, Enter your base Salary for some operations: ");
         baseSalary = salariesDetails.nextDouble();
         System.out.print("Please dear employee, Enter your incentive per hour for some operations: ");
         workPerHour = salariesDetails.nextDouble();
-        System.out.print("Please dear employee, Enter your additions hours for some operations: ");
+        System.out.print("Please dear employee, Enter your work hours for some operations: ");
         BonusHour = salariesDetails.nextDouble();
         ///
-        taxes = baseSalary * taxesPercentage;
-        incentives = workPerHour * BonusHour;
+        double baseSalaryCondition = 30_000.0;
+        if (baseSalary <= baseSalaryCondition) {
+            taxPercent = 0.05;
+        }
+        taxes = baseSalary * taxPercent;
+        if (workPerHour >= 15) {
+            incentives = workPerHour * BonusHour + 500;
+        } else {
+            incentives = workPerHour * BonusHour;
+        }
+        ;
         netSalary = (baseSalary + incentives) - taxes;
         ///
         System.out.println("your base Salary is equal to: \t" + baseSalary);
@@ -32,8 +45,16 @@ public class Main {
         ///
         System.out.println("-----------------");
         System.out.println("Net Salary:\t " + netSalary + "EGP");
+//
 
-
+//
+        System.out.println("Target Details \n");
+        double monthTargrt = salariesDetails.nextDouble();
+        double targetPercent = .05;
+        for (int i = 1; i < 13; i++) {
+            System.out.println("month  " + i + "  :" + (int) monthTargrt);
+            monthTargrt += (monthTargrt * targetPercent);
+        }
     }
 }
 
